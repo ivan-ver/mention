074b48c96c1d0335004760ae0bbec28382a26b98
Ivan.Vereschagin <Ivan.Vereschagin@casceng.com>
2024-03-29 21:02:22 +0300

    init

---
+++ src/main/java/ru/plant/mention/MentionApplication.java
@@ -0,0 +1,13 @@
+package ru.plant.mention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentionApplication.class, args);
+    }
+
+}
